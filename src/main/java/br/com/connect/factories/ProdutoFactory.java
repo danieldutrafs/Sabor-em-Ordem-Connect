70e787a5eac15d9985df8d3d1bc0c2358e477b19
Daniel Dutra <darthur.dev@gmail.com>
2026-07-29 10:36:03 -0300

    Organiza pacotes e classse Main

---
+++ src/main/java/br/com/connect/factories/ProdutoFactory.java
@@ -0,0 +1,15 @@
+package br.com.connect.factories;
+
+import br.com.connect.models.Categoria;
+import br.com.connect.models.Produto;
+
+/**
+ *
+ * @author Daniel Dutra
+ */
+public class ProdutoFactory {
+
+    public static Produto criarProduto(long id, String nomeProduto, int quantidade, double preco, Categoria categoria) {
+        return new Produto(id, nomeProduto, quantidade, preco, categoria);
+    }
+}
